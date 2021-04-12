9e4e1faa1550c97ed5fa8eefa9cbc2eae55be687
Polela <Polela@HSTCPT-POLELA.HST.local>
2021-04-12 15:23:55 +0200

    New class committing

---
+++ MyfirstRepo/src/main/java/com/org/MyfirstRepo/NeweClassDemoget.java
@@ -0,0 +1,15 @@
+package com.org.MyfirstRepo;
+
+public class NeweClassDemoget {
+
+	public NeweClassDemoget() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Git class");
+
+	}
+
+}
